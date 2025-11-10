26990063165ecd39499cf96e2b06b337f8ce5fcf
Yehuda Pinchas <100363046+rdwr-yehudap@users.noreply.github.com>
2025-11-10 14:01:39 +0200

    Update hook.sh

--- hook.sh
+++ hook.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Primary Cyber Controller parameters
-PRIMARY_VDIRECT_IP_PORT="10.0.0.100:2189"
-PRIMARY_VDIRECT_USER="root"
-PRIMARY_VDIRECT_PASS="${primary_cc_password_for_ACME}"
+PRIMARY_CC_IP="10.0.0.1"
+PRIMARY_CC_USER="ACME-User"
+PRIMARY_CC_PASS="${primary_cc_password_for_ACME}"
 
 # Secondary Cyber Controller parameters (Optional)
-SECONDARY_VDIRECT_IP_PORT=""
-SECONDARY_VDIRECT_USER=""
-SECONDARY_VDIRECT_PASS="" #"${secondary_cc_password_for_ACME}"
+SECONDARY_CC_IP=""
+SECONDARY_CC_USER=""
+SECONDARY_CC_PASS="" #"${secondary_cc_password_for_ACME}"
 
 INSECURE=true
 ALTEONS=${ALTEON_DEVICES}
@@ -48,57 +48,88 @@ function deploy_challenge {
 EOF
 
     # Function to perform curl request for challenge deployment
-    perform_challenge_curl() {
-        local url=$1
+    perform_curl() {
+        local CC_IP=$1
         local username=$2
         local password=$3
-        curl -s -o /dev/null -w "%{http_code}" -u $username:$password "https://$url/api/runnable/ConfigurationTemplate/Alteon_Deploy_ACME_Challenge.vm/run/sync" \
-          -H "Content-Type: application/json" \
-          --data "$DATA" $CURL_OPTIONS
+        local temp_response
+
+        echo "Logging in to Cyber-Controller"
+        CC_JSESSION=$(curl -s -X POST "https://$CC_IP/mgmt/system/user/login" -H 'Content-Type: application/json' --data '{"username": "'"$username"'", "password": "'"$password"'"}' $CURL_OPTIONS | jq -r '.jsessionid')
+
+        echo "CC_JSESSION is:"
+        echo $CC_JSESSION
+
+        # Capture both response body and status code
+        temp_response=$(curl -s -w "%{http_code}" "https://$CC_IP:2189/api/runnable/ConfigurationTemplate/Alteon_Deploy_ACME_Challenge.vm/run/sync" \
+            -H "content-type: application/json" \
+            -b "JSESSIONID=$CC_JSESSION" \
+            $CURL_OPTIONS \
+            --data "$DATA")
+
+        # Extract the status code and response body
+        RESPONSE_BODY="${temp_response::-3}" # Remove the last 3 characters (status code)
+        RESPONSE_CODE="${temp_response: -3}" # Extract the last 3 characters (status code)
+
+        if [ -n "$CC_JSESSION" ]; then
+            echo "Logging out from Cyber-Controller"
+            curl "https://$CC_IP/mgmt/system/user/logout" \
+                -H 'content-type: application/json;charset=UTF-8' \
+                -b "JSESSIONID=$CC_JSESSION" \
+                --data-raw '{"headers":{"Content-Type":"application/json","Cache-Control":"no-cache"}}' \
+                $CURL_OPTIONS
+        fi
     }
 
     # Array of URLs and their respective credentials
-    urls_credentials=("$PRIMARY_VDIRECT_IP_PORT $PRIMARY_VDIRECT_USER $PRIMARY_VDIRECT_PASS" "$SECONDARY_VDIRECT_IP_PORT $SECONDARY_VDIRECT_USER $SECONDARY_VDIRECT_PASS")
+    urls_credentials=("$PRIMARY_CC_IP $PRIMARY_CC_USER $PRIMARY_CC_PASS" "$SECONDARY_CC_IP $SECONDARY_CC_USER $SECONDARY_CC_PASS")
 
     # Attempt to use each URL up to 3 times
     max_attempts=3
     for entry in "${urls_credentials[@]}"; do
-        read url username password <<< "$entry"
-        if [ -n "$url" ]; then
+        read CC_IP username password <<< "$entry"
+        if [ -n "$CC_IP" ]; then
             attempt=0
             while [ $attempt -lt $max_attempts ]; do
                 attempt=$((attempt+1))
-                response_code=$(perform_challenge_curl "$url" "$username" "$password")
+                perform_curl "$CC_IP" "$username" "$password"
+                # echo $response | jq -r .
 
-                if [ "$response_code" -eq 200 ]; then
-                    echo "Attempt $attempt: Successful response received from $url for challenge deployment"
+                echo "response_code $RESPONSE_CODE"
+
+
+                if [[ "$RESPONSE_CODE" -eq 200 ]]; then
+                    echo "Attempt $attempt: Successful response received from $CC_IP"
+
                     break 2 # Exit both loops on success
+
                 else
-                    echo "Attempt $attempt: Failed with response code $response_code"
+                    echo "Attempt $attempt: Failed with response code ${RESPONSE_CODE:-unknown}"
+                    echo "$RESPONSE_BODY"
                 fi
 
                 if [ $attempt -eq $max_attempts ]; then
-                    echo "Failed to get a successful response from $url after $max_attempts attempts for challenge deployment"
+                    echo "Failed to get a successful response from $CC_IP after $max_attempts attempts"
                 fi
 
                 # Optional: Add a delay between attempts
-                sleep 1
+                sleep 10
             done
         fi
     done
 
     # Check if the final attempt failed
-    if [ "$response_code" -ne 200 ]; then
-        if [ -n "$SECONDARY_VDIRECT_IP_PORT" ]; then
-            echo "Failed deploying challenge on Alteons - both cyber-controllers did not return 200 OK"
+    if [[ $RESPONSE_CODE -ne 200 ]]; then
+        if [ -n "$SECONDARY_CC_IP" ]; then
+            echo "Failed cleaning challenge from Alteons - both cyber-controllers did not return 200 OK"
         else
-            echo "Failed deploying challenge on Alteons, the first cyber-controller did not return 200 OK, and no secondary cyber-controller configured"
+            echo "Failed cleaning challenge from Alteons, the first cyber-controller did not return 200 OK, and no secondary cyber-controller configured"
         fi
-        exit 1
+
     else
-        echo "Challenge has been successfully deployed."
+        echo "Challenge has been successfully deployed"
     fi
-    sleep 10
+    sleep 5
 }
 
 
@@ -127,57 +158,89 @@ function clean_challenge {
 }
 EOF
 
-    # Function to perform curl request for challenge deployment
-    perform_challenge_curl() {
-        local url=$1
+
+    perform_curl() {
+        local CC_IP=$1
         local username=$2
         local password=$3
-        curl -s -o /dev/null -w "%{http_code}" -u $username:$password "https://$url/api/runnable/ConfigurationTemplate/Alteon_Clean_ACME_Challenge.vm/run/sync" \
-          -H "Content-Type: application/json" \
-          --data "$DATA" $CURL_OPTIONS
+        local temp_response
+
+        echo "Logging in to Cyber-Controller"
+        CC_JSESSION=$(curl -s -X POST "https://$CC_IP/mgmt/system/user/login" -H 'Content-Type: application/json' --data '{"username": "'"$username"'", "password": "'"$password"'"}' $CURL_OPTIONS | jq -r '.jsessionid')
+
+        echo "CC_JSESSION is:"
+        echo $CC_JSESSION
+
+        # Capture both response body and status code
+        temp_response=$(curl -s -w "%{http_code}" "https://$CC_IP:2189/api/runnable/ConfigurationTemplate/Alteon_Clean_ACME_Challenge.vm/run/sync" \
+            -H "content-type: application/json" \
+            -b "JSESSIONID=$CC_JSESSION" \
+            $CURL_OPTIONS \
+            --data "$DATA")
+
+        # Extract the status code and response body
+        RESPONSE_BODY="${temp_response::-3}" # Remove the last 3 characters (status code)
+        RESPONSE_CODE="${temp_response: -3}" # Extract the last 3 characters (status code)
+
+        if [ -n "$CC_JSESSION" ]; then
+            echo "Logging out from Cyber-Controller"
+            curl "https://$CC_IP/mgmt/system/user/logout" \
+                -H 'content-type: application/json;charset=UTF-8' \
+                -b "JSESSIONID=$CC_JSESSION" \
+                --data-raw '{"headers":{"Content-Type":"application/json","Cache-Control":"no-cache"}}' \
+                $CURL_OPTIONS
+        fi
     }
 
     # Array of URLs and their respective credentials
-    urls_credentials=("$PRIMARY_VDIRECT_IP_PORT $PRIMARY_VDIRECT_USER $PRIMARY_VDIRECT_PASS" "$SECONDARY_VDIRECT_IP_PORT $SECONDARY_VDIRECT_USER $SECONDARY_VDIRECT_PASS")
+   urls_credentials=("$PRIMARY_CC_IP $PRIMARY_CC_USER $PRIMARY_CC_PASS" "$SECONDARY_CC_IP $SECONDARY_CC_USER $SECONDARY_CC_PASS")
 
-    # Attempt to use each URL up to 3 times
-    max_attempts=3
-    for entry in "${urls_credentials[@]}"; do
-        read url username password <<< "$entry"
-        if [ -n "$url" ]; then
-            attempt=0
-            while [ $attempt -lt $max_attempts ]; do
-                attempt=$((attempt+1))
-                response_code=$(perform_challenge_curl "$url" "$username" "$password")
+   # Attempt to use each URL up to 3 times
+   max_attempts=3
+   for entry in "${urls_credentials[@]}"; do
+       read CC_IP username password <<< "$entry"
+       if [ -n "$CC_IP" ]; then
+           attempt=0
+           while [ $attempt -lt $max_attempts ]; do
+               attempt=$((attempt+1))
+               perform_curl "$CC_IP" "$username" "$password"
+               # echo $response | jq -r .
 
-                if [ "$response_code" -eq 200 ]; then
-                    echo "Attempt $attempt: Successful response received from $url for cleaning challenge"
-                    break 2 # Exit both loops on success
-                else
-                    echo "Attempt $attempt: Failed with response code $response_code"
-                fi
+               echo "response_code $RESPONSE_CODE"
 
-                if [ $attempt -eq $max_attempts ]; then
-                    echo "Failed to get a successful response from $url after $max_attempts attempts for cleaning challenge"
-                fi
 
-                # Optional: Add a delay between attempts
-                sleep 1
-            done
-        fi
-    done
+               if [[ "$RESPONSE_CODE" -eq 200 ]]; then
+                   echo "Attempt $attempt: Successful response received from $CC_IP"
 
-    # Check if the final attempt failed
-    if [ "$response_code" -ne 200 ]; then
-        if [ -n "$SECONDARY_VDIRECT_IP_PORT" ]; then
-            echo "Failed cleaning challenge from Alteons - both cyber-controllers did not return 200 OK"
-        else
-            echo "Failed cleaning challenge from Alteons, the first cyber-controller did not return 200 OK, and no secondary cyber-controller configured"
-        fi
-        exit 1
-    else
-        echo "Challenge has been successfully cleaned."
-    fi
+                   break 2 # Exit both loops on success
+
+               else
+                   echo "Attempt $attempt: Failed with response code ${RESPONSE_CODE:-unknown}"
+                   echo "$RESPONSE_BODY"
+               fi
+
+               if [ $attempt -eq $max_attempts ]; then
+                   echo "Failed to get a successful response from $CC_IP after $max_attempts attempts"
+               fi
+
+               # Optional: Add a delay between attempts
+               sleep 10
+           done
+       fi
+   done
+
+   # Check if the final attempt failed
+   if [[ $RESPONSE_CODE -ne 200 ]]; then
+       if [ -n "$SECONDARY_CC_IP" ]; then
+           echo "Failed cleaning challenge from Alteons - both cyber-controllers did not return 200 OK"
+       else
+           echo "Failed cleaning challenge from Alteons, the first cyber-controller did not return 200 OK, and no secondary cyber-controller configured"
+       fi
+
+   else
+       echo "Challenge has been successfully cleaned"
+   fi
+   sleep 5
 }
 
 
@@ -191,6 +254,10 @@ function deploy_cert {
     # echo $KEY_CONTENT
     # echo $CERT_CONTENT
     # echo $CHAIN_CONTENT
+
+    # Get the serial number of the cert
+    SERIAL_NEW_CERT=$(openssl x509 -in ${CERTFILE} -noout -serial | cut -d'=' -f2)
+    echo "SERIAL_NEW_CERT $SERIAL_NEW_CERT"
 
     # Avoid * in the Alteon certificate name because this is a special character
     if [[ ${DOMAIN:0:1} == "*" ]]; then
@@ -213,6 +280,7 @@ function deploy_cert {
 {
   "__dryRun": false,
   "alteons": $ALTEONS_JSON,
+  "serialNewCert": "${SERIAL_NEW_CERT}",
   "name": "${ALTEON_CERT_NAME}",
   "key": "${KEY_CONTENT}",
   "password": "",
@@ -221,64 +289,90 @@ function deploy_cert {
 }
 EOF
 
+
     # Function to perform curl request
     perform_curl() {
-        local url=$1
+        local CC_IP=$1
         local username=$2
         local password=$3
-        curl -s -o /dev/null -w "%{http_code}" -u $username:$password "https://$url/api/runnable/ConfigurationTemplate/Alteon_Deploy_Certificate.vm/run/sync" \
-          -H "accept: application/json, text/plain, */*" \
-          -H "accept-language: en-US,en;q=0.9" \
-          -H "content-type: application/json" \
-          -H "sec-ch-ua: \"Google Chrome\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"" \
-          -H "sec-ch-ua-mobile: ?0" \
-          -H "sec-ch-ua-platform: \"Windows\"" \
-          -H "sec-fetch-dest: empty" \
-          -H "sec-fetch-mode: cors" \
-          -H "sec-fetch-site: same-origin" \
-          -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36" \
-          $CURL_OPTIONS \
-          --data "$DATA"
+        local temp_response
+
+        echo "Logging in to Cyber-Controller"
+        CC_JSESSION=$(curl -s -X POST "https://$CC_IP/mgmt/system/user/login" -H 'Content-Type: application/json' --data '{"username": "'"$username"'", "password": "'"$password"'"}' $CURL_OPTIONS | jq -r '.jsessionid')
+
+        echo "CC_JSESSION is:"
+        echo $CC_JSESSION
+
+        # Capture both response body and status code
+        temp_response=$(curl -s -w "%{http_code}" "https://$CC_IP:2189/api/runnable/ConfigurationTemplate/Alteon_Deploy_Certificate.vm/run/sync" \
+            -H "content-type: application/json" \
+            -b "JSESSIONID=$CC_JSESSION" \
+            $CURL_OPTIONS \
+            --data "$DATA")
+
+        # Extract the status code and response body
+        RESPONSE_BODY="${temp_response::-3}" # Remove the last 3 characters (status code)
+        RESPONSE_CODE="${temp_response: -3}" # Extract the last 3 characters (status code)
+
+        if [ -n "$CC_JSESSION" ]; then
+            echo "Logging out from Cyber-Controller"
+            curl "https://$CC_IP/mgmt/system/user/logout" \
+                -H 'content-type: application/json;charset=UTF-8' \
+                -b "JSESSIONID=$CC_JSESSION" \
+                --data-raw '{"headers":{"Content-Type":"application/json","Cache-Control":"no-cache"}}' \
+                $CURL_OPTIONS
+        fi
     }
 
     # Array of URLs and their respective credentials
-    urls_credentials=("$PRIMARY_VDIRECT_IP_PORT $PRIMARY_VDIRECT_USER $PRIMARY_VDIRECT_PASS" "$SECONDARY_VDIRECT_IP_PORT $SECONDARY_VDIRECT_USER $SECONDARY_VDIRECT_PASS")
+    urls_credentials=("$PRIMARY_CC_IP $PRIMARY_CC_USER $PRIMARY_CC_PASS" "$SECONDARY_CC_IP $SECONDARY_CC_USER $SECONDARY_CC_PASS")
 
     # Attempt to use each URL up to 3 times
     max_attempts=3
     for entry in "${urls_credentials[@]}"; do
-        read url username password <<< "$entry"
-        if [ -n "$url" ]; then
+        read CC_IP username password <<< "$entry"
+        if [ -n "$CC_IP" ]; then
             attempt=0
             while [ $attempt -lt $max_attempts ]; do
                 attempt=$((attempt+1))
-                response_code=$(perform_curl "$url" "$username" "$password")
+                perform_curl "$CC_IP" "$username" "$password"
+                # echo $response | jq -r .
 
-                if [ "$response_code" -eq 200 ]; then
-                    echo "Attempt $attempt: Successful response received from $url"
+                echo "response_code $RESPONSE_CODE"
+
+
+                if [[ "$RESPONSE_CODE" -eq 200 ]]; then
+                    echo "Attempt $attempt: Successful response received from $CC_IP"
+
+                    if [ -z "$cert_status" ]; then
+                        cert_status=$(echo "$RESPONSE_BODY" | jq -r '.parameters.output // "No output key found"')
+                        echo $cert_status
+                    fi
+
                     break 2 # Exit both loops on success
+
                 else
-                    echo "Attempt $attempt: Failed with response code $response_code"
+                    echo "Attempt $attempt: Failed with response code ${RESPONSE_CODE:-unknown}"
+                    echo "$RESPONSE_BODY"
                 fi
 
                 if [ $attempt -eq $max_attempts ]; then
-                    echo "Failed to get a successful response from $url after $max_attempts attempts"
+                    echo "Failed to get a successful response from $CC_IP after $max_attempts attempts"
                 fi
 
                 # Optional: Add a delay between attempts
-                sleep 1
+                sleep 10
             done
         fi
     done
 
     # Check if the final attempt failed
-    if [ "$response_code" -ne 200 ]; then
-        if [ -n "$SECONDARY_VDIRECT_IP_PORT" ]; then
+    if [[ $RESPONSE_CODE -ne 200 ]]; then
+        if [ -n "$SECONDARY_CC_IP" ]; then
             echo "Failed deploying cert on Alteons - both cyber-controllers did not return 200 OK"
         else
             echo "Failed deploying cert on Alteons, the first cyber-controller did not return 200 OK, and no secondary cyber-controller configured"
         fi
-        exit 1
     else
         echo "Certificate deployment succeeded."
     fi
@@ -287,7 +381,8 @@ EOF
 
     SANs=$(openssl x509 -in "${CERTFILE}" -noout -text | grep -oP '(?<=DNS:)[^,]*')
     for DOMAIN_FROM_CERT in $SANs; do
-        jq --arg domain "$DOMAIN_FROM_CERT" --arg value "Success" '. + {($domain): $value}' "$JSON_FILE_CERTS_STATUS" > "$JSON_FILE_CERTS_STATUS.tmp" && mv "$JSON_FILE_CERTS_STATUS.tmp" "$JSON_FILE_CERTS_STATUS"
+        jq --arg domain "$DOMAIN_FROM_CERT" --arg value "$cert_status" '. + {($domain): $value}' "$JSON_FILE_CERTS_STATUS" > "$JSON_FILE_CERTS_STATUS.tmp" && mv "$JSON_FILE_CERTS_STATUS.tmp" "$JSON_FILE_CERTS_STATUS"
+
     done
 }
 
@@ -310,6 +405,9 @@ function unchanged_cert {
     #   The path of the file containing the full certificate chain.
     # - CHAINFILE
     #   The path of the file containing the intermediate certificate(s).
+
+    deploy_cert "$DOMAIN" "$KEYFILE" "$CERTFILE" "$FULLCHAINFILE" "$CHAINFILE"
+
 }
 
 invalid_challenge() {
@@ -354,4 +452,3 @@ HANDLER="$1"; shift
 if [[ "${HANDLER}" =~ ^(deploy_challenge|clean_challenge|deploy_cert|unchanged_cert|invalid_challenge|request_failure|exit_hook)$ ]]; then
   "$HANDLER" "$@"
 fi
-
